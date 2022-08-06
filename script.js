658f97c5caa14d6153235441d8c405cff900900f
Luz Karime Karime <109636149+Luzk35@users.noreply.github.com>
2022-08-06 03:15:26 +0000

    punto de partida

---
+++ script.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', function (){
+  //aca estoy selecionado el dom de slir
+  const  imagenes = [];
+  // estamos creando un array
+  imagenes[0] = 'media/luzka1.jpg'
+  imagenes[1] = 'media/hero.jpg'
+  imagenes[2] = 'media/manito.jpg'
+
+
+  function cambiarimg(){
+    document.slider.src =imagenes[];
+    if(indicesImagenes < 2 ){
+      
+    }
+    
+  }
+
+  
+  
+});
